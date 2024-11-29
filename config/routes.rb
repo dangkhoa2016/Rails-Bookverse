4e964e664392633dd6ecd7c4c637ee01ab917184
dangkhoa2016 <i.am@dangkhoa.dev>
2024-11-29 10:24:28 +0000

    add home page and navigation menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   resources :book_categories
   resources :reviews
   resources :authors
@@ -9,12 +11,17 @@ Rails.application.routes.draw do
   resources :book_loans
   resources :members
   resources :libraries
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :settings, only: %i[index] do
+    collection do
+      post :update
+    end
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
 end
