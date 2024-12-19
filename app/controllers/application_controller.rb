2579e60b9c3fb38ae54f99b790d86790b4dad699
dangkhoa2016 <i.am@dangkhoa.dev>
2024-12-19 14:15:01 +0000

    add ability to switch locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    locale = get_locale
+    I18n.locale = locale
+    cookies[:locale] = { value: locale, expires: 1.year.from_now }
+  end
+
+  def get_locale
+    locale = params[:locale]
+    if locale && I18n.available_locales.include?(locale.to_sym)
+      locale
+    else
+      cookies[:locale] || I18n.default_locale
+    end
+  end
 end
