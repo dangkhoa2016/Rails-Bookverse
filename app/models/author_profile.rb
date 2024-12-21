b68605da06900f56caa142b34bbb4eda6e200b77
dangkhoa2016 <i.am@dangkhoa.dev>
2024-12-21 08:33:28 +0000

    update form and locale

--- app/models/author_profile.rb
+++ app/models/author_profile.rb
@@ -2,4 +2,16 @@ class AuthorProfile < ApplicationRecord
   belongs_to :author
 
   validates :bio, presence: true
+
+
+  class << self
+
+    def human_attribute_name(attribute, options = {})
+      if attribute == 'author' && options[:base].respond_to?(:author) && options[:base].author.blank?
+        attribute = 'author_id'
+      end
+
+      super(attribute, options)
+    end
+  end
 end
--- manual/ruby.rb
+++ manual/ruby.rb
@@ -14,3 +14,10 @@ tables.each do |table|
   next if klass.nil?
   puts "#{table}: #{klass.count}"
 end
+
+# I18n locale
+ActiveModel::Error.generate_message 'title', 'blank', Book.new, {}
+ActiveModel::Error.full_message 'title', 'blank', Book.new
+I18n.translate 'errors.template.header', model: 'Book', count: 1
+I18n.translate 'errors.template.header', model: 'Book', count: 4
+I18n.translate 'actions.confirm_destroy', model: 'Book', count: 4
