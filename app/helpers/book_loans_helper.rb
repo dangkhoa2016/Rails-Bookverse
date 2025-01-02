280ece4c9be3ea41c7ca0efa98ee8cb20e17ba09
dangkhoa2016 <i.am@dangkhoa.dev>
2025-01-02 22:43:29 +0000

    dynamic render form

--- app/helpers/book_loans_helper.rb
+++ app/helpers/book_loans_helper.rb
@@ -2,4 +2,8 @@ module BookLoansHelper
   def render_status_value(value)
     BookLoan.human_attribute_name("status.#{value}")
   end
+
+  def render_status_form_field(form)
+    form.select(:status, BookLoan.status_collection_for_select, {}, class: 'form-select')
+  end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,7 @@
 
 pin 'application'
 pin '@hotwired/turbo-rails', to: 'turbo.min.js'
+# pin '@hotwired/turbo-rails', to: 'turbo.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin 'app_theme', to: 'app_theme.js'
