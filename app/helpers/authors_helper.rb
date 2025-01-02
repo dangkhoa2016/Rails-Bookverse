59bf6d92396f0281daed7043669f9725fd4ddebe
dangkhoa2016 <i.am@dangkhoa.dev>
2025-01-02 22:43:29 +0000

    dynamic render form

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -1,2 +1,7 @@
 module AuthorsHelper
+  def authors_options_for_select
+    Author.all.map do |author|
+      [author.full_name, author.id]
+    end
+  end
 end
--- app/helpers/genres_helper.rb
+++ app/helpers/genres_helper.rb
@@ -1,2 +1,7 @@
 module GenresHelper
+  def genres_options_for_select
+    Genre.all.map do |genre|
+      [genre.name, genre.id]
+    end
+  end
 end
