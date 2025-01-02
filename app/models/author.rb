5f343ad08b18c48587fe46b7b3aa8ec5d1d3b14a
dangkhoa2016 <i.am@dangkhoa.dev>
2025-01-02 07:03:45 +0000

    dynamic render record

--- app/models/author.rb
+++ app/models/author.rb
@@ -5,7 +5,37 @@ class Author < ApplicationRecord
   validates :first_name, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
 
+  attr_accessor :author_profiles_count, :books_count
+
+
   def full_name
     [first_name, last_name].compact.join(' ')
+  end
+
+  def display_author_profiles_count
+    author_profiles_count || author_profiles.count
+  end
+
+  def display_books_count
+    books_count || books.count
+  end
+
+  def to_s
+    full_name
+  end
+
+
+  class << self
+
+    def display_columns
+      [
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'display_author_profiles_count', 'display_books_count',
+        'active', 'created_at', 'updated_at'
+      ]
+    end
   end
 end
--- app/models/author_profile.rb
+++ app/models/author_profile.rb
@@ -3,15 +3,25 @@ class AuthorProfile < ApplicationRecord
 
   validates :bio, presence: true
 
+  def to_s
+    author&.full_name
+  end
+
 
   class << self
 
-    def human_attribute_name(attribute, options = {})
-      if attribute == 'author' && options[:base].respond_to?(:author) && options[:base].author.blank?
-        attribute = 'author_id'
-      end
-
-      super(attribute, options)
+    def display_columns
+      [
+        'id',
+        {
+          field: 'author',
+          type: 'association',
+        },
+        'bio',
+        'social_facebook', 'social_twitter', 'social_instagram', 'social_linkedin', 'social_youtube',
+        'active', 'created_at', 'updated_at'
+      ]
     end
+
   end
 end
