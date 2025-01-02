350fd6b8418b0a504afd75a57559b61f9cd1956e
dangkhoa2016 <i.am@dangkhoa.dev>
2025-01-02 07:03:45 +0000

    dynamic render record

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,19 @@
 module ReviewsHelper
+  def render_rating_value(value)
+    one_star = raw(
+      <<-SVG
+      <svg class='icon text-warning'>
+        <use xlink:href="#{asset_path('coreui-free.svg')}#cil-star"></use>
+      </svg>
+      SVG
+    )
+
+    value = value&.to_i
+
+    if value > 0
+      raw("<span class='d-inline-block' title='#{Review.human_attribute_name('star', { count: value })}'>#{one_star * value}</span>")
+    else
+      Review.human_attribute_name('no_rating')
+    end
+  end
 end
--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -3,4 +3,33 @@ class Publisher < ApplicationRecord
   has_many :books, through: :published_dates
 
   validates :name, presence: true, uniqueness: true
+
+  attr_accessor :published_dates_count
+
+
+  def to_s
+    name
+  end
+
+  def display_published_dates_count
+    published_dates_count || published_dates.count
+  end
+
+
+  class << self
+
+    def display_columns
+      [
+        'id',
+        {
+          field: 'name',
+          only_in_form: true,
+        },
+        'address', 'phone_number', 'email',
+        'website', 'established_year', 'ceo_name',
+        'display_published_dates_count',
+        'active', 'created_at', 'updated_at',
+      ]
+    end
+  end
 end
