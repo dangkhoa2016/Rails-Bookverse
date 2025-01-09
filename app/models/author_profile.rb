916111213b273d72aa86841953f658a5022dbc95
dangkhoa2016 <i.am@dangkhoa.dev>
2025-01-09 02:57:01 +0000

    refactor social network fields

--- app/models/author_profile.rb
+++ app/models/author_profile.rb
@@ -12,6 +12,10 @@ class AuthorProfile < ApplicationRecord
     author&.full_name
   end
 
+  def social_networks
+    attributes.slice('social_facebook', 'social_twitter', 'social_instagram', 'social_linkedin', 'social_youtube')
+  end
+
 
   class << self
 
@@ -24,7 +28,30 @@ class AuthorProfile < ApplicationRecord
           display_by_actions: ['index', 'show', 'edit', 'update', 'new', 'create'],
         },
         'bio',
-        'social_facebook', 'social_twitter', 'social_instagram', 'social_linkedin', 'social_youtube',
+        {
+          field: 'social_facebook',
+          display_by_actions: ['edit', 'update', 'new', 'create'],
+        },
+        {
+          field: 'social_twitter',
+          display_by_actions: ['edit', 'update', 'new', 'create'],
+        },
+        {
+          field: 'social_instagram',
+          display_by_actions: ['edit', 'update', 'new', 'create'],
+        },
+        {
+          field: 'social_linkedin',
+          display_by_actions: ['edit', 'update', 'new', 'create'],
+        },
+        {
+          field: 'social_youtube',
+          display_by_actions: ['edit', 'update', 'new', 'create'],
+        },
+        {
+          field: 'social_networks',
+          display_by_actions: ['show', 'index'],
+        },
         'active', 'created_at', 'updated_at'
       ]
     end
