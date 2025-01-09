56bc0877176eaaf25f9486bc54490841bacee1eb
dangkhoa2016 <i.am@dangkhoa.dev>
2025-01-09 02:57:01 +0000

    refactor social network fields

--- app/models/author_profile.rb
+++ app/models/author_profile.rb
@@ -15,6 +15,10 @@ class AuthorProfile < ApplicationRecord
     author&.full_name
   end
 
+  def social_networks
+    attributes.slice("social_facebook", "social_twitter", "social_instagram", "social_linkedin", "social_youtube")
+  end
+
 
   class << self
     def human_attribute_name(attribute, options = {})
@@ -34,14 +38,37 @@ class AuthorProfile < ApplicationRecord
           display_by_actions: [ "index", "show", "edit", "update", "new", "create" ]
         },
         "bio",
-        "social_facebook", "social_twitter", "social_instagram", "social_linkedin", "social_youtube",
+        {
+          field: "social_facebook",
+          display_by_actions: [ "edit", "update", "new", "create" ]
+        },
+        {
+          field: "social_twitter",
+          display_by_actions: [ "edit", "update", "new", "create" ]
+        },
+        {
+          field: "social_instagram",
+          display_by_actions: [ "edit", "update", "new", "create" ]
+        },
+        {
+          field: "social_linkedin",
+          display_by_actions: [ "edit", "update", "new", "create" ]
+        },
+        {
+          field: "social_youtube",
+          display_by_actions: [ "edit", "update", "new", "create" ]
+        },
+        {
+          field: "social_networks",
+          display_by_actions: [ "show", "index" ]
+        },
         "active", "created_at", "updated_at"
       ]
     end
 
     def count_by_author_ids(ids)
       model = "author"
-      column_name = "#{model}_id"
+      column_name = "#{model}_id".to_sym
       AuthorProfile.where(column_name => ids).group(column_name).count
     end
   end
